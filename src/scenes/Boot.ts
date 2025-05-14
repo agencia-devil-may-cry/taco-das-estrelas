c6c9b8c43d4d059764b8285dfd1b480c5f72b4f5
nicky-web217 <nicolas.hm2006@aluno.ifsc.edu.br>
2025-05-14 18:29:58 +0000

    adicionar imagem

--- src/scenes/Boot.ts
+++ src/scenes/Boot.ts
@@ -4,4 +4,12 @@ export class Boot extends Scene {
     constructor() {
         super({ key: 'Boot' });
     }
+
+    preload() {
+        this.load.image('taco-das-estrelas', 'assets/taco-nas-estrelas.jpg');
+    }
+
+    create() {
+        this.add. image(400, 225, 'taco-das-estrelas');
+    }
 }
