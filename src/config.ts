32155f1161274bf6bba6fdbe26db2757ac3290c3
wasto4 <149070352+wasto4@users.noreply.github.com>
2025-05-21 18:23:18 +0000

    adicionar segunda cena

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,12 @@
-import { Types } from
-    "phaser"
-import { Boot } from
-    "./scenes/Boot";
+import { Types } from    "phaser";
+import { Boot } from    "./scenes/Boot";
+import { Preloader } from "./scenes/Preloader";
 
 const config: Types.Core.GameConfig = {
   type: Phaser.AUTO,
   width: 800,
   height: 450,
   parent: "game-container",
-    scene: [Boot],
+    scene: [Boot, Preloader],
 };
 export default config;
